7c708c1eedc924ddaae2dca2fde4277e3502d524
Marc S. Brooks <marc@mbrooks.info>
2023-07-25 07:00:11 -0700

    Use MSFT supported Docker image
    Workaround container constraints

--- templates/ecosystem.config.js
+++ templates/ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [{
-    script: '{{appName}}/node_modules/lambda-edge-server/server.js --handler {{appName}}/src/app.js',
-    error_file : '.devcontainer/error.log',
-    out_file : '.devcontainer/debug.log',
+    script: 'cd {{appName}}; node_modules/lambda-edge-server/server.js --handler src/app.js',
+    error_files: '.devcontainer/error.log',
+    out_file: '.devcontainer/debug.log',
     watch: ['{{appName}}/src'],
     watch_delay: 1000
   }]
