f1df02b4fd13e26aba3a7c479478a052d1e5a4dc
Marc S. Brooks <marc@mbrooks.info>
2023-10-04 11:37:59 -0700

    Updated PM2 logs output location
    Removed directory from watch

--- templates/ecosystem.config.js
+++ templates/ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
     script: 'node_modules/lambda-edge-server/server.js',
     args: '--handler src/app.js',
     cwd: '{{appName}}',
-    error_files: '.devcontainer/error.log',
-    out_file: '.devcontainer/debug.log',
-    watch: ['{{appName}}/src'],
+    error_file: '../.devcontainer/error.log',
+    out_file: '../.devcontainer/debug.log',
+    watch: ['src'],
     watch_delay: 5000,
     instances: 1,
     exec_mode: 'fork'
