31f49b841ad2de68973ba614693a667d87bc09bc
Marc S. Brooks <marc@mbrooks.info>
2024-05-30 16:23:14 -0700

    Skip CLI args parser when imported

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-require('./dist/cli');
+if (!module.parent) {
+  return require('./dist/cli');
+}
 
 const {createFiles, createFile}  = require('./dist/generator');
 const {addPackage, listPackages} = require('./dist/installer');
